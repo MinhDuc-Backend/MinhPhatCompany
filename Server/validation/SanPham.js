89c1aa038a9965b8737fdbfe69ee081cef218ef9
Nguyễn Văn Minh Đức <minhduc140401@gmail.com>
2024-08-23 16:24:47 +0700

    update

--- Server/validation/SanPham.js
+++ Server/validation/SanPham.js
@@ -6,7 +6,6 @@ export const KtraDuLieuKhiThem = data => {
     error.isRequired(data.MaSP, "MaSP")
         .isRequired(data.MaLSPCha, "MaLSPCha")
         .isRequired(data.TenSP, "TenSP")
-        .isRequired(data.Hinh, "Hinh")
         .isRequired(data.SoLuong, "SoLuong")
 
     return error.get()
@@ -16,11 +15,7 @@ export const KtraDuLieuKhiChinhSua = data => {
     const error = new Error()
 
     error.isRequired(data.MaLSPCha, "MaLSPCha")
-        .isRequired(data.MaLSPCon, "MaLSPCon")
         .isRequired(data.TenSP, "TenSP")
-        .isRequired(data.Hinh, "Hinh")
-        .isRequired(data.Gia, "Gia")
-        .isRequired(data.MoTa, "MoTa")
         .isRequired(data.SoLuong, "SoLuong")
 
     return error.get()
